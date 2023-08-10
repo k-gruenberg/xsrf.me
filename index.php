cb1c42a8d004771a14164ffa2441159e8b72b99e
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2023-08-10 13:53:28 +0200

    changed regex yet again, to [^ -~], regex is now also check in HTML as [ -~]*

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 	} elseif (isset($_POST["post_message_submit"]) && isset($_SESSION["login"]) && $_SESSION["login"] == "true") {
 		// Add posted message to DB:
 		$statement = $db->prepare("INSERT INTO messages(id, username, ip_addr, message, timestmp) VALUES ((SELECT IFNULL(MAX(id)+1, 0) FROM (SELECT * FROM messages) AS m), ?, ?, ?, CURRENT_TIMESTAMP)");
-		$statement->execute(array($_SESSION["username"], $_SERVER['REMOTE_ADDR'], substr(preg_replace("/[^a-zA-Z0-9 _\-.,;]/i", "", $_POST["message"]), 0, 64)));
+		$statement->execute(array($_SESSION["username"], $_SERVER['REMOTE_ADDR'], substr(preg_replace("/[^ -~]/i", "", $_POST["message"]), 0, 64)));
 	}
 ?>
 <!DOCTYPE html>
@@ -89,7 +89,7 @@
 						<?php
 							if (isset($_SESSION["login"]) && $_SESSION["login"] === "true") {
 								echo "<form action=\"index.php\" method=\"POST\">
-									<label for=\"message\">Message: </label><input type=\"text\" id=\"message\" name=\"message\" />
+									<label for=\"message\">Message: </label><input type=\"text\" id=\"message\" name=\"message\" pattern=\"[ -~]*\" />
 									<input type=\"submit\" name=\"post_message_submit\" value=\"Post message\" />
 								</form>";
 							} else {
